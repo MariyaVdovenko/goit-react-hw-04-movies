91518f86e8a7d0708ba5a205a2bb37f1fb16e8f8
MariyaVdovenko <m.vdovenko@ukr.net>
2020-01-09 09:27:02 +0200

    Updates

---
+++ precache-manifest.95c5547b39d2c715eb4d937eef87d5e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa69da2ed00f4f065e2c007a58cb6da",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "addac9ecc377b56b8f81",
+    "url": "/goit-react-hw-04-movies/static/css/main.91bb1a95.chunk.css"
+  },
+  {
+    "revision": "8cd7b44fc1616022a540",
+    "url": "/goit-react-hw-04-movies/static/js/2.24ccb25c.chunk.js"
+  },
+  {
+    "revision": "addac9ecc377b56b8f81",
+    "url": "/goit-react-hw-04-movies/static/js/main.330dabb9.chunk.js"
+  },
+  {
+    "revision": "8d2254b44b6d7071638a",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.0824e930.js"
+  }
+]);
